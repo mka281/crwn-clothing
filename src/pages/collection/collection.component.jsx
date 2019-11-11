15be6032dab2b8e45b4ff30ee073e24242045a23
Muhammed Kilicarslan <mka281@icloud.com>
2019-11-11 09:43:14 -0500

    Data normalization on collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 import { selectShopCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
 
-const CollectionPage = ({ collection }) => (
-  <div className="collection">COLLECTION PAGE</div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return (
+    <div className="collection-page">
+      <div className="title">{title}</div>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-const mapStateToProps = (state, ownProps) =>
-  createStructuredSelector({
-    collection: selectShopCollection(ownProps.match.params.collectionId)
-  });
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectShopCollection(ownProps.match.params.collectionId)(state)
+});
 
 export default connect(mapStateToProps)(CollectionPage);
