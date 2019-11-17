c1e8e0bf3d2600e188d0bd593085368556a41b7b
Muhammed Kilicarslan <mka281@icloud.com>
2019-11-17 10:40:13 -0500

    fix: font styles error due to the import order of css files

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
-import App from "./App";
+
 import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <Provider store={store}>
